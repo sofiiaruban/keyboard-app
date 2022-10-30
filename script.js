0acf7185bb3ddcea84f6c89cf219bab23997ef69
Sofiia Ruban <37212452+sofiiaruban@users.noreply.github.com>
2022-10-30 18:49:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function colorBtn(event) {
     let pressedKeyCell = document.querySelector(`.key[data-key="${val}"]`);
 
     for (let i = 0; i < keys.length; i++) {
-    
         keys[i].classList.remove('active');
-
     }
     pressedKeyCell.classList.add('active');
     pressedKey.value = '';
